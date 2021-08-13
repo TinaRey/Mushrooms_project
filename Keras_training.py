721146feec1abfe8b3b1a47de9b61948fd357145
TinaRey <tinarey110@gmail.com>
2021-08-13 17:28:09 +0200

    Hydra setup, tensorboard setup, callbacks checkpoints

--- Keras_training.py
+++ Keras_training.py
@@ -1,121 +1,125 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-import matplotlib.pyplot as plt
 import os
-from PIL import Image
+import logging
+
+import tensorflow as tf
+from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.image import smart_resize
 from tensorflow.keras.models import Sequential
-import numpy as np
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+import hydra
+from omegaconf import DictConfig
 
 
 AUTOTUNE = tf.data.AUTOTUNE
 # helper functions
 
-def get_label(file_path):
-  parts = tf.strings.split(file_path, os.path.sep)
-  one_hot = parts[-2] == class_names
-  return tf.argmax(one_hot)
 
-def decode_img(img):
-  img = tf.io.decode_jpeg(img, channels=3)
-  return smart_resize(img, img_size)
-
-def process_path(file_path):
-  label = get_label(file_path)
-  img = tf.io.read_file(file_path)
-  img = decode_img(img)
-  return img, label
+def get_label(file_path, class_names):
+    parts = tf.strings.split(file_path, os.path.sep)
+    one_hot = parts[-2] == class_names
+    return tf.argmax(one_hot)
 
 
-class_names = ['Agaricus', 'Amanita', 'Boletus', 'Cortinarius', 'Entoloma', 'Hygrocybe', 'Lactarius', 'Russula', 'Suillus']
-data_dir = "./Mushrooms"
-img_size = (128, 128)
-batch_size = 64
-list_ds = tf.data.Dataset.list_files(data_dir+"/*/*.jpg", shuffle=True)
-image_count = list_ds.cardinality().numpy()
-
-val_size = int(image_count * 0.2)
-test_size = int(image_count * 0.01)
-train_ds = list_ds.skip(val_size+test_size)
-val_ds = list_ds.take(val_size)
-test_ds = list_ds.take(test_size)
+def decode_img(img, target_img_size):
+    img = tf.io.decode_jpeg(img, channels=3)
+    return smart_resize(img, target_img_size)
 
 
-train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-test_ds = test_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-
-train_ds = train_ds.batch(batch_size)
-val_ds = val_ds.batch(batch_size)
-test_ds = test_ds.batch(test_ds.cardinality().numpy())
-
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+def process_path(file_path, class_names, target_img_size):
+    label = get_label(file_path, class_names)
+    img = tf.io.read_file(file_path)
+    img = decode_img(img, target_img_size)
+    return img, label
 
 
-normalization_layer = layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_size[0], img_size[1], 3))
+def simple_model(nb_classes, input_size):
+    normalization_layer = layers.experimental.preprocessing.Rescaling(
+        1./255, input_shape=(input_size[0], input_size[1], 3))
+    model = Sequential([
+        normalization_layer,
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(nb_classes)
+    ])
 
-num_classes = len(class_names)
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                      from_logits=True),
+                  metrics=['accuracy'])
+    return model
 
-model = Sequential([
-  normalization_layer,
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
-])
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+@hydra.main(config_path="config", config_name="config.yaml")
+def main_func(cfg: DictConfig):
+    log = logging.getLogger(__name__)
+    log.debug("Debug level message")
+    log.info("Info level message")
+    log.warning("Warning level message")
 
-model.summary()
-epochs=10
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
-)
+    orig_cwd = hydra.utils.get_original_cwd()
+    data_folder = cfg.dataset.data_folder
+    data_dir = os.path.join(orig_cwd, data_folder)
+    class_names = os.listdir(data_dir)
+    target_img_size = (cfg.dataset.target_img_size, cfg.dataset.target_img_size)
+    batch_size = cfg.batch_size
+    val_size = cfg.val_size
+    test_size = cfg.test_size
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+    list_ds = tf.data.Dataset.list_files(data_dir+"/*/*.jpg", shuffle=True)
+    image_count = list_ds.cardinality().numpy()
+    val_size = int(image_count * val_size)
+    test_size = int(image_count * test_size)
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+    train_ds = list_ds.skip(val_size+test_size)
+    val_ds = list_ds.take(val_size)
+    test_ds = list_ds.take(test_size)
 
-epochs_range = range(epochs)
+    train_ds = train_ds.map(lambda x: process_path(
+        x, class_names, target_img_size), num_parallel_calls=AUTOTUNE)
+    val_ds = val_ds.map(lambda x: process_path(
+        x, class_names, target_img_size), num_parallel_calls=AUTOTUNE)
+    test_ds = test_ds.map(lambda x: process_path(
+        x, class_names, target_img_size), num_parallel_calls=AUTOTUNE)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+    train_ds = train_ds.batch(batch_size)
+    val_ds = val_ds.batch(batch_size)
+    test_ds = test_ds.batch(test_ds.cardinality().numpy())
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-print(test_ds.cardinality().numpy())
+    nb_classes = len(class_names)
+    input_size = target_img_size
+    model = simple_model(nb_classes, input_size)
 
-nb_imgs = 8
+    epochs = 10
+    checkpoint_path = "ckpt_files/cp.ckpt"
 
-plt.figure(figsize=(nb_imgs, nb_imgs))
-for data, labels in test_ds.take(nb_imgs):
-  predictions = model.predict(data)
-  for i in range(nb_imgs):
-    score = tf.nn.softmax(predictions[i])
-    ax = plt.subplot(int(np.ceil(nb_imgs/2)), int(np.ceil(nb_imgs/2)), i + 1)
-    plt.imshow(data[i].numpy().astype("uint8"))
-    plt.title("True class: {} Class predicted: {} with {:.2f}".format(class_names[labels[i]], class_names[np.argmax(score)], 100 * np.max(score)))
-    plt.axis("off")
-plt.show()
+    cp_callback = ModelCheckpoint(filepath=checkpoint_path,
+                                  save_weights_only=True,
+                                  save_best_only=True,
+                                  verbose=1)
+
+    log_dir = "logs/fit/"
+    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs,
+        callbacks=[cp_callback, tensorboard_callback]
+    )
+
+    # Evaluate the model
+    loss, acc = model.evaluate(test_ds, verbose=2)
+    print("Trained model, accuracy: {:5.2f}%".format(100 * acc))    
+
+
+if __name__ == "__main__":
+    main_func()
--- tests.py
+++ tests.py
@@ -18,17 +18,3 @@ for folder in os.listdir(data_dir):
         f = os.path.join(folder_path, filename)
         if verify_image(f)==False:
             print(f+str(verify_image(f)))
-
-
-#Display images and their label:
-# plt.figure(figsize=(4, 4))
-# for data, labels in train_ds.take(1):
-#     print(data.shape)
-#     print(labels.shape)
-#     for i in range(4):
-#         print(data[i].shape)
-#         ax = plt.subplot(2, 2, i + 1)
-#         plt.imshow(data[i].numpy().astype("uint8"))
-#         plt.title(class_names[labels[i]])
-#         plt.axis("off")
-# plt.show()
